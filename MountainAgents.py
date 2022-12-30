4d290ad3c1c84e148f39bf2491098eaa984be4bb
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2022-12-30 16:56:42 +0000

    Update MountainAgents.py

--- MountainAgents.py
+++ MountainAgents.py
@@ -17,7 +17,6 @@ if __name__ == '__main__':
     dqn_params = dict(layers=layers, activation=activation, weights=weights, optim=optim, learning_rate=learning_rate)
 
     # Training Parameters
-    num_runs = 1
     epsilon = 1
     eps_decay = 0.996  # Epsilon is reduced by 1-eps_decay every episode
     replay_buffer = 100000
